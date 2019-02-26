557ee1eaefe296a0acdd733caecb588a71bbfb7c
Igor Kanel <igor.kanel@gmail.com>
2019-02-25 22:03:42 -0500

    GPIOZero was replaced with RPI.GPIO

--- PythonPi/gpio.py
+++ PythonPi/gpio.py
@@ -1,29 +1,34 @@
-from gpiozero import LED, Button
+import RPi.GPIO as GPIO
 from time import sleep
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 def set_io_pins(pins):
     for pin in pins.split(","):
-        pinnum=pin.split("|")[0]
+        pinnum=int(pin.split("|")[0])
         pinval=pin.split("|")[1]
-        led = LED(pinnum)
+        GPIO.setup(pinnum,GPIO.OUT)
+    
         if(pinval == "0"):
-            led.off()
+            GPIO.output(pinnum,GPIO.LOW)
+            print("pin "+str(pinnum)+" off")
         else:
-            led.on()
+            GPIO.output(pinnum,GPIO.HIGH)
+            print("pin "+str(pinnum)+" on")
 
 
 def get_io_pins(pins):
     res=[]
     for pin in pins.split(","):
-        btn = Button(pin)
-        r=str(pin)+"|";
-        if(btn.is_pressed):
-            r+="1"
-        else:
+        pinnum=int(pin)
+        GPIO.setup(pinnum, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        r=pin+"|";
+        if(GPIO.input(pinnum)):
             r+="0"
+        else:
+            r+="1"
         res.append(r)
     return ",".join(res)
 
-#print(get_io_pins("1,2,3,4,5,6"))
-#set_io_pins("1|1,2|0")
+print(get_io_pins("1,2,3,4,5,6"))
+#set_io_pins("1|1,2|1")
